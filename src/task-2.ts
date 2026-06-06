42ae102fe179daba5a492f7634b94c34e6b9a564
Roman Koplyk <romasve94@gmail.com>
2026-06-06 19:29:52 +0200

    remove global block and verify interface

--- src/task-2.ts
+++ src/task-2.ts
@@ -1,15 +1,13 @@
-{
-  type Product = {
-    readonly id: number;
-    title: string;
-    description?: string;
-  };
-
-  const product: Product = {
-    id: 1,
-    title: "Tablet",
-    description: "Compact and fast",
-  };
-
-  console.log(`Product: ${JSON.stringify(product)}`);
+interface Product {
+  readonly id: number;
+  title: string;
+  description?: string;
 }
+
+const product: Product = {
+  id: 1,
+  title: "Tablet",
+  description: "Compact and fast",
+};
+
+console.log(`Product: ${JSON.stringify(product)}`);
--- src/task-6.ts
+++ src/task-6.ts
@@ -1,9 +1,10 @@
-{
-  function getFirstElement<T>(arr: T[]): T {
-    return arr[0];
-  }
-
-  console.log("Task 6 (numbers):", getFirstElement([1, 2, 3])); // Виведе: 1
-  console.log("Task 6 (strings):", getFirstElement(["a", "b", "c"])); // Виведе: "a"
-  console.log("Task 6 (booleans):", getFirstElement([true, false, true])); // Виведе: true
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
 }
+
+console.log("Task 6 (numbers):", getFirstElement<number>([1, 2, 3])); // Виведе: 1
+console.log("Task 6 (strings):", getFirstElement<string>(["a", "b", "c"])); // Виведе: "a"
+console.log(
+  "Task 6 (booleans):",
+  getFirstElement<boolean>([true, false, true]),
+); // Виведе: true
